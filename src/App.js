76f806cd5af3e844c73e1367a4ab9e78e75ca22c
mustafasrbs <mustafasrbs1@gmail.com>
2023-11-06 22:16:13 +0300

    product pages
    axios ile veritabanından aldığımız verileri listeler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Content from "./components/content/Content";
 import Navbar from "./components/navbar/Navbar";
+import './App.css'
 
 function App() {
   return (<>
-  <div>mustafa</div>
     <Navbar />
-    <Content />
+    <div className="App">
+    <Content 
+    img ="https://cdn.bubilet.com.tr/files/Etkinlik/ajda-pekkan--48333.jpg"
+    title="Ajda Pekkan"
+    description="Congresium Ankara 22 Aralık Cuma - 21.00"
+    />
+      <Content 
+    img ="https://cdn.bubilet.com.tr/files/Etkinlik/melike-sahin-konseri-27604.png"
+    title="Melike Şahin"
+    description="Congresium Ankara 19 Aralık Salı - 19.00"
+    />
+      <Content 
+    img ="https://cdn.bubilet.com.tr/files/Etkinlik/mabel-matiz-konseri--96310.png"
+    title="Mabel Matiz"
+    description="Congresium Ankara 13 Aralık Çarşamba - 21.00"
+    />
+      </div>
   </>);
 }
 
