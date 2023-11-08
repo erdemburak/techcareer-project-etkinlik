d3c3cb2eedfc536f54a6772d9e3127aeb9d05f51
erdemburak <burakerdem6@hotmail.com>
2023-11-08 21:41:05 +0300

    Navbar background değişti

--- src/components/content/navbar/Navbar.jsx
+++ src/components/content/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
     return (<>
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" >
+            <nav className="navbar navbar-expand-lg navbar-light " style={{ backgroundColor: '#F2F2F2' }} >
                 <a className="navbar-brand" href="#" style={{ paddingLeft: '2%' }}>
                     Navbar
                 </a>
