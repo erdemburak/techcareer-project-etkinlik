16dd3de6406336c4b294dd842bfbdce0ba7e9705
mustafasrbs <mustafasrbs1@gmail.com>
2023-11-12 18:04:01 +0300

    etkinlik arama
    arama motorundan konser adı adres gibi özellikler aratılır

--- src/components/content/pages/ShowEtkinlik.jsx
+++ src/components/content/pages/ShowEtkinlik.jsx
@@ -1,10 +1,11 @@
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 // import './card.css';
-// import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+// import { Button, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
 // function ShowEtkinlik() {
 //     const [events, setEvents] = useState([]);
+//     const [selectedDate, setSelectedDate] = useState('');
 
 //     useEffect(() => {
 //         axios.get('http://51.20.118.182/api/v1/etkinlik/list')
@@ -16,75 +17,67 @@
 //             });
 //     }, []);
 
-//     return (<>
+//     const filteredEvents = events.filter(event => {
+//         if (selectedDate) {
+//             const startDate = new Date(selectedDate);
+//             const endDate = new Date(selectedDate);
+//             endDate.setDate(endDate.getDate() + 1); 
 
-//         <div style={{ backgroundColor: '#F2F2F2', padding: '20px' }}>
-//             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', paddingLeft: '3%', paddingRight: '3%' }}>
-//                 <h1 style={{ color: '#336699' }}>Etkinlik Listesi</h1>
-//                 <h1>Filter</h1>
+//             return (
+//                 new Date(event.etkinlikBaslangic) >= startDate && new Date(event.etkinlikBitis) < endDate
+//             );
+//         }
+//         return true; 
+//     });
+
+//     return (
+//         <>
+//             <div style={{ backgroundColor: '#F2F2F2', padding: '20px' }}>
+//                 <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', paddingLeft: '3%', paddingRight: '3%' }}>
+//                     <h1 style={{ color: '#336699' }}>Etkinlik Listesi</h1>
+//                     <input type="date" onChange={(e) => setSelectedDate(e.target.value)} />
+//                 </div>
+//                 <hr />
+
+//                 <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+//                     {filteredEvents.map(event => (
+//                         <div key={event.id} style={{ width: '300px', margin: '16px' }}>
+//                             <Card style={{ backgroundColor: 'white' }}>
+//                                 <CardActionArea>
+//                                     <CardMedia
+//                                         component="img"
+//                                         height="200"
+//                                         image={event.gorselUrl}
+//                                         title={event.aciklama}
+//                                         style={{ borderRadius: "4%" }}
+//                                     />
+//                                     <CardContent>
+//                                         <Typography variant="h6" gutterBottom>
+//                                             {event.aciklama}
+//                                         </Typography>
+//                                         <Typography variant="body2" color="textSecondary">
+//                                             Konum: {event.konumAdi}
+//                                         </Typography>
+//                                         <Typography variant="body2" color="textSecondary">
+//                                             Adres: {event.adres}
+//                                         </Typography>
+//                                         <Typography variant="body2" color="textSecondary">
+//                                             Tür: {event.etkinlikType}
+//                                         </Typography>
+//                                         <Typography variant="body2" color="textSecondary">
+//                                             Başlangıç: {event.etkinlikBaslangic}
+//                                         </Typography>
+//                                         <Typography variant="body2" color="textSecondary">
+//                                             Bitiş: {event.etkinlikBitis}
+//                                         </Typography>
+//                                     </CardContent>
+//                                 </CardActionArea>
+//                             </Card>
+//                         </div>
+//                     ))}
+//                 </div>
 //             </div>
-//             <hr />
-
-//             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-//                 {events.map(event => (
-//                     <div key={event.id} style={{ width: '300px', margin: '16px' }}>
-//                         <Card style={{ backgroundColor: 'white' }}>
-//                             <CardActionArea>
-//                                 <CardMedia
-//                                     component="img"
-//                                     height="200"
-//                                     image={event.gorselUrl}
-//                                     title={event.aciklama}
-//                                     style={{ borderRadius: "4%" }}
-//                                 />
-//                                 <CardContent>
-//                                     <Typography variant="h6" gutterBottom>
-//                                         {event.aciklama}
-//                                     </Typography>
-//                                     <Typography variant="body2" color="textSecondary">
-//                                         Konum: {event.konumAdi}
-//                                     </Typography>
-//                                     <Typography variant="body2" color="textSecondary">
-//                                         Adres: {event.adres}
-//                                     </Typography>
-//                                     <Typography variant="body2" color="textSecondary">
-//                                         Tür: {event.etkinlikType}
-//                                     </Typography>
-//                                     <Typography variant="body2" color="textSecondary">
-//                                         Başlangıç: {event.etkinlikBaslangic}
-//                                     </Typography>
-//                                     <Typography variant="body2" color="textSecondary">
-//                                         Bitiş: {event.etkinlikBitis}
-//                                     </Typography>
-//                                     {/* <Typography variant="body2">
-//                                         <strong>Ücretler:</strong>
-//                                     </Typography>
-//                                     <ul>
-//                                         {event.etkinlikUcretleri.map(ucret => (
-
-//                                             <li key={ucret.id}>{ucret.kategoriType}: {ucret.fiyat} TL</li>
-//                                         ))}
-//                                     </ul>
-//                                     <Typography variant="body2">
-//                                         <strong>Resimler:</strong>
-//                                     </Typography>
-//                                     <ul>
-//                                         {event.etkinlikResimleri.map(resim => (
-//                                             <li key={resim.id}>{resim.resimAd}</li>
-//                                         ))}
-//                                     </ul> */}
-//                                 </CardContent>
-//                             </CardActionArea>
-//                             <CardActionArea style={{ display: 'flex', justifyContent: 'space-between' }}>
-//                                 <Button size="small" style={{ backgroundColor: '#F2F2F2' }}>Paylaş</Button>
-//                                 <Button size="small" style={{ backgroundColor: '#F2F2F2' }}>Detay</Button>
-//                             </CardActionArea>
-//                         </Card>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     </>
+//         </>
 //     );
 // }
 
@@ -97,6 +90,7 @@ import { Button, Card, CardActionArea, CardContent, CardMedia, Typography } from
 function ShowEtkinlik() {
     const [events, setEvents] = useState([]);
     const [selectedDate, setSelectedDate] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         axios.get('http://51.20.118.182/api/v1/etkinlik/list')
@@ -112,13 +106,23 @@ function ShowEtkinlik() {
         if (selectedDate) {
             const startDate = new Date(selectedDate);
             const endDate = new Date(selectedDate);
-            endDate.setDate(endDate.getDate() + 1); 
+            endDate.setDate(endDate.getDate() + 1);
 
             return (
-                new Date(event.etkinlikBaslangic) >= startDate && new Date(event.etkinlikBitis) < endDate
+                new Date(event.etkinlikBaslangic) >= startDate &&
+                new Date(event.etkinlikBitis) < endDate
             );
         }
-        return true; 
+
+        if (searchTerm) {
+            return (
+                (event.aciklama && event.aciklama.toLowerCase().includes(searchTerm.toLowerCase())) ||
+                (event.yapanGrup && event.yapanGrup.toLowerCase().includes(searchTerm.toLowerCase())) ||
+                (event.yapanKisi && event.yapanKisi.toLowerCase().includes(searchTerm.toLowerCase()))
+            );
+        }
+
+        return true;
     });
 
     return (
@@ -127,12 +131,15 @@ function ShowEtkinlik() {
                 <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', paddingLeft: '3%', paddingRight: '3%' }}>
                     <h1 style={{ color: '#336699' }}>Etkinlik Listesi</h1>
                     <input type="date" onChange={(e) => setSelectedDate(e.target.value)} />
+                    <input type="text" placeholder="Etkinlik veya Grup Ara" onChange={(e) => setSearchTerm(e.target.value)} />
                 </div>
                 <hr />
 
+                {/* Etkinlik Kartları */}
                 <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
                     {filteredEvents.map(event => (
                         <div key={event.id} style={{ width: '300px', margin: '16px' }}>
+                            {/* Kart Detayları */}
                             <Card style={{ backgroundColor: 'white' }}>
                                 <CardActionArea>
                                     <CardMedia
