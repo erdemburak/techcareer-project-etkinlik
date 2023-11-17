6d4aa6dc528e8bd3caed4d5df6b308c0e227dbc5
erdemburak <burakerdem6@hotmail.com>
2023-11-17 23:47:07 +0300

    favorilerden çıkarma işlemi yapıldı

--- src/components/content/pages/FavoriEtkinlikler.jsx
+++ src/components/content/pages/FavoriEtkinlikler.jsx
@@ -1,60 +1,72 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, CardActionArea, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import Navbar from '../navbar/Navbar';
+import { removeFavoriteEvent } from '../favorite/favoriteEventsSlice';
 
 function FavoriEtkinlikler() {
+    const dispatch = useDispatch();
     const favoriteEvents = useSelector(state => state.favoriteEvents);
-
+    const handleRemoveFromFavorites = (eventId) => {
+        // Dispatch the action to remove the event from favorites
+        dispatch(removeFavoriteEvent(eventId));
+    };
 
     return (<>
-        <div>
-            <h2>Favori Etkinlikler</h2>
-            {favoriteEvents.map(event => (
-                <div key={event.id} style={{ width: '300px', margin: '16px' }}>
-                    {/* Kart Detayları */}
-                    <Card style={{ backgroundColor: 'white' }}>
-                        <CardActionArea>
-                            <Link to={`/etkinlik/${event.id}`} style={{ textDecoration: 'none' }}>
-                                <CardMedia
-                                    component="img"
-                                    height="200"
-                                    src={event.etkinlikResimleri[0].resimAd}
-                                    title={event.aciklama}
-                                    style={{ borderRadius: "4%" }}
-                                /> </Link>
-                            <CardContent>
-                                <Typography variant="h6" gutterBottom>
-                                    {event.ad}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Konum: {event.konumAdi}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Tür: {event.etkinlikType}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Başlangıç: {moment(event.etkinlikBaslangic).format('DD/MM/YYYY HH:mm')}
-                                </Typography>
-                                <Typography variant="body2" color="textSecondary">
-                                    Bitiş: {moment(event.etkinlikBitis).format('DD/MM/YYYY HH:mm')}
-                                </Typography>
+        <Navbar />
+        <div style={{ backgroundColor: '#F2F2F2', padding: '20px' }}>
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', paddingLeft: '3%', paddingRight: '3%' }}>
+                <h2 style={{ color: '#336699' }}>Favori Etkinlikler</h2>
+            </div>
+            <hr />
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+                {favoriteEvents.map(event => (
+                    <div key={event.id} style={{ width: '300px', margin: '16px' }}>
+                        {/* Kart Detayları */}
+                        <Card style={{ backgroundColor: 'white' }}>
+                            <CardActionArea>
+                                <Link to={`/etkinlik/${event.id}`} style={{ textDecoration: 'none' }}>
+                                    <CardMedia
+                                        component="img"
+                                        height="200"
+                                        src={event.etkinlikResimleri[0].resimAd}
+                                        title={event.aciklama}
+                                        style={{ borderRadius: "4%" }}
+                                    /> </Link>
+                                <CardContent>
+                                    <Typography variant="h6" gutterBottom>
+                                        {event.ad}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Konum: {event.konumAdi}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Tür: {event.etkinlikType}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Başlangıç: {moment(event.etkinlikBaslangic).format('DD/MM/YYYY HH:mm')}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Bitiş: {moment(event.etkinlikBitis).format('DD/MM/YYYY HH:mm')}
+                                    </Typography>
 
-                            </CardContent>
-                        </CardActionArea>
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon color={favoriteEvents.some(favEvent => favEvent.id === event.id) ? 'secondary' : 'default'} />
-                        </IconButton>
-                        <IconButton aria-label="share">
-                            <ShareIcon />
-                        </IconButton>
+                                </CardContent>
+                            </CardActionArea>
+                            <IconButton aria-label="add to favorites" onClick={() => handleRemoveFromFavorites(event.id)}>
+                                <FavoriteIcon color={favoriteEvents.some(favEvent => favEvent.id === event.id) ? 'secondary' : 'default'} />
+                            </IconButton>
+                            <IconButton aria-label="share">
+                                <ShareIcon />
+                            </IconButton>
 
-                    </Card>
-                </div>
-            ))}
+                        </Card>
+                    </div>
+                ))}
+            </div>
         </div>
     </>);
 }
