0c9a0fee35f8816572edc29c82cad73eb00bfa4e
erdemburak <burakerdem6@hotmail.com>
2023-11-17 21:12:25 +0300

    EskiEtkinlikler sayfası için Route eklendi

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import CreateEtkinlik from '../adminPanel/adminPages/CreateEtkinlik'
 import AdminMainPage from '../adminPanel/adminPages/AdminMainPage'
 import EtkinlikDetail from './pages/EtkinlikDetail'
+import EskiEtkinlikler from './pages/EskiEtkinlikler'
 
 
 function Content() {
@@ -11,6 +12,7 @@ function Content() {
 
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/eskiEtkinlikler" element={<EskiEtkinlikler />} />
       <Route path="/etkinlik/:id" element={<EtkinlikDetail />} />
       <Route path="/admin" element={<AdminMainPage />} />
       <Route path="/admin/create" element={<CreateEtkinlik />} />
